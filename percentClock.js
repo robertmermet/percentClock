036f1715a0b9f725d39e102814d7e47fb7850fb4
robert mermet <robertmermet@gmail.com>
2018-05-30 23:30:09 -0400

    code clean up

--- percentClock.js
+++ percentClock.js
@@ -6,7 +6,50 @@ window.addEventListener('load', function load() {
         fullYear   = 31536000,
         dayNames   = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
         monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        clocks     = ['day', 'month', 'year'];
+        clocks     = ['day', 'month', 'year'],
+        canvas     = [],
+        context    = [],
+        j          = 0;
+
+    while (j <= 2) {
+        canvas[j]  = document.getElementById( clocks[j] + 'Canvas');
+        context[j] = canvas[j].getContext('2d');
+        context[j].translate(100, 100);
+        context[j].rotate(-90 * Math.PI / 180);
+        j++;
+    }
+
+    (function drawFrame() {
+        requestAnimationFrame(drawFrame);
+        updateClock();
+    })();
+
+    function updateClock() {
+        var i = 0;
+        percentTime = determineValues();
+        while (i <= 2) {
+            printTime(i, percentTime);
+            drawCanvas(i, percentTime);
+            i++;
+        }
+    }
+
+    function printTime(i, percentTime) {
+        var thisTime   = (percentTime[0][i] * 100).toFixed(2) + '%',
+            time       = document.getElementById(clocks[i] + 'Time'),
+            text       = document.getElementById(clocks[i] + 'Text').getElementsByTagName('span')[0];
+        time.innerHTML = thisTime;
+        text.innerHTML = percentTime[1][i];
+    }
+
+    function drawCanvas(i, percentTime) {
+        var thisPercentTime = [];
+        thisPercentTime[i]  = percentTime[0][i] * 2;
+        context[i].beginPath();
+        context[i].clearRect(-100, -100, 200, 200);
+        context[i].arc(0, 0, 90, 0, thisPercentTime[i] * Math.PI);
+        context[i].stroke();
+    }
 
     function determineValues() {
         var newDate     = new Date(),
@@ -45,47 +88,5 @@ window.addEventListener('load', function load() {
 
         return percentTime;
     }
-
-    function printTime(i, percentTime) {
-        var thisTime   = (percentTime[0][i] * 100).toFixed(2) + '%',
-            time       = document.getElementById(clocks[i] + 'Time'),
-            text       = document.getElementById(clocks[i] + 'Text').getElementsByTagName('span')[0];
-        time.innerHTML = thisTime;
-        text.innerHTML = percentTime[1][i];
-    }
-
-    var canvas  = [],
-        context = [],
-        j       = 0;
-
-    while (j <= 2) {
-        canvas[j]  = document.getElementById( clocks[j] + 'Canvas');
-        context[j] = canvas[j].getContext('2d');
-        context[j].translate(100, 100);
-        context[j].rotate(-90 * Math.PI / 180);
-        j++;
-    }
-
-    function drawCanvas(i, percentTime) {
-        var thisPercentTime = [];
-        thisPercentTime[i]  = percentTime[0][i] * 2;
-        context[i].beginPath();
-        context[i].clearRect(-100, -100, 200, 200);
-        context[i].arc(0, 0, 90, 0, thisPercentTime[i] * Math.PI);
-        context[i].stroke();
-    }
-
-    function updateClock() {
-        var i = 0;
-        percentTime = determineValues();
-        while (i <= 2) {
-            printTime(i, percentTime);
-            drawCanvas(i, percentTime);
-            i++;
-        }
-    }
-
-    updateClock();
-    setInterval(updateClock, 1000);
 
 }, true);
